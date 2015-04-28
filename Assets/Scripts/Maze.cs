e01b1638ab6f22e535b656449cff817036bbb16b
DharcRune <dharcrose@gmail.com>
2015-04-28 14:35:57 -0600

    The maze now has archways in it.

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -9,7 +9,13 @@ public class Maze : MonoBehaviour
 	public IntVector2 size;
 	public MazePassage passagePrefab;
 	public MazeWall wallPrefab;
+	public MazeDoor doorPrefab;
+	public MazeRoomSettings[] roomSettings;
 	private MazeCell[,] cells;
+	private List<MazeRoom> rooms = new List<MazeRoom>();
+
+	[Range(0f, 1f)]
+	public float doorProbability;
 
 	public MazeCell GetCell(IntVector2 coordinates)
 	{
@@ -31,7 +37,9 @@ public class Maze : MonoBehaviour
 
 	private void DoFirstGenerationStep(List<MazeCell> activeCells)
 	{
-		activeCells.Add(CreateCell(RandomCoordinates));
+		MazeCell newCell = CreateCell(RandomCoordinates);
+		newCell.Initialize(CreateRoom(-1));
+		activeCells.Add(newCell);
 	}
 
 	private void DoNextGenerationStep(List<MazeCell> activeCells)
@@ -58,6 +66,10 @@ public class Maze : MonoBehaviour
 				CreatePassage(currentCell, neighbor, direction);
 				activeCells.Add(neighbor);
 			}
+			else if(currentCell.room == neighbor.room)
+			{
+				CreatePassageInSameRoom(currentCell, neighbor, direction);
+			}
 			else
 			{
 				CreateWall(currentCell, neighbor, direction);
@@ -71,7 +83,28 @@ public class Maze : MonoBehaviour
 
 	private void CreatePassage(MazeCell cell, MazeCell otherCell, MazeDirection direction)
 	{
-		MazePassage passage = Instantiate (passagePrefab) as MazePassage;
+		MazePassage prefab = Random.value < doorProbability ? doorPrefab : passagePrefab;
+
+		MazePassage passage = Instantiate(prefab) as MazePassage;
+		passage.Initialize(cell, otherCell, direction);
+
+		passage = Instantiate(prefab) as MazePassage;
+
+		if(prefab is MazePassage)
+		{
+			otherCell.Initialize(CreateRoom(cell.room.settingsIndex));
+		}
+		else 
+		{
+			otherCell.Initialize(cell.room);
+		}
+
+		passage.Initialize(otherCell, cell, direction.GetOpposite());
+	}
+
+	private void CreatePassageInSameRoom(MazeCell cell, MazeCell otherCell, MazeDirection direction)
+	{
+		MazePassage passage = Instantiate(passagePrefab) as MazePassage;
 		passage.Initialize(cell, otherCell, direction);
 
 		passage = Instantiate(passagePrefab) as MazePassage;
@@ -101,6 +134,22 @@ public class Maze : MonoBehaviour
 		newCell.transform.localPosition = new Vector3 (coordinates.x - size.x * 0.5f + 0.5f, 0f, coordinates.z - size.z * 0.5f + 0.5f);
 
 		return newCell;
+	}
+
+	private MazeRoom CreateRoom(int indexToExclude)
+	{
+		MazeRoom newRoom = ScriptableObject.CreateInstance<MazeRoom>();
+		newRoom.settingsIndex = Random.Range(0, roomSettings.Length);
+
+		if(newRoom.settingsIndex == indexToExclude)
+		{
+			newRoom.settingsIndex = (newRoom.settingsIndex + 1) % roomSettings.Length;
+		}
+
+		newRoom.settings = roomSettings[newRoom.settingsIndex];
+		rooms.Add(newRoom);
+
+		return newRoom;
 	}
 
 	public IntVector2 RandomCoordinates
